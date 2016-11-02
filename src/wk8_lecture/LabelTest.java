0edede505c3eb8b105d3ca00a4924f66206448a3
Kevin Ma <kbma.kevin@gmail.com>
2016-11-02 09:29:31 -0400

    ex1

---
+++ src/wk8_lecture/LabelTest.java
@@ -0,0 +1,16 @@
+package wk8_lecture;
+
+import java.awt.Window;
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+	public static void main(String[] args) {
+		LabelFrame lFrame = new LabelFrame();
+		lFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		lFrame.setSize(260, 180);
+		lFrame.setVisible(true);
+		lFrame.setResizable(false);
+		
+	}
+}
