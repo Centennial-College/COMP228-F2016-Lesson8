210e5472a78f60619dfb6ef5e116de4f415fec62
Kevin Ma <kbma.kevin@gmail.com>
2016-11-04 19:40:40 -0400

    example 5

---
+++ src/wk8_lab/RadioButtonTest.java
@@ -0,0 +1,14 @@
+package wk8_lab;
+
+import javax.swing.JFrame;
+
+public class RadioButtonTest {
+	public static void main(String[] args) {
+		RadioButtonFrame rFrame = new RadioButtonFrame();
+
+		rFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		rFrame.setSize(325, 100);
+		rFrame.setVisible(true);
+
+	}
+}
