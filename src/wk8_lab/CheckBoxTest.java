c3381df583785dc97dfd17a467657d6ffb555020
Kevin Ma <kbma.kevin@gmail.com>
2016-11-04 19:18:40 -0400

    example4

---
+++ src/wk8_lab/CheckBoxTest.java
@@ -0,0 +1,13 @@
+package wk8_lab;
+
+import javax.swing.JFrame;
+
+public class CheckBoxTest {
+	public static void main(String[] args) {
+		CheckBoxFrame cFrame = new CheckBoxFrame();
+
+		cFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		cFrame.setVisible(true);
+		cFrame.setSize(275, 100);
+	}
+}
