7ad36a15f5949df287d5e55bd80014a52285e14c
Kevin Ma <kbma.kevin@gmail.com>
2016-11-04 20:16:52 -0400

    example1 from week9

---
+++ src/wk8_lab/ComboBoxTest.java
@@ -0,0 +1,12 @@
+package wk8_lab;
+
+import javax.swing.JFrame;
+
+public class ComboBoxTest {
+	public static void main(String[] args) {
+		ComboBoxFrame cBFrame = new ComboBoxFrame();
+		cBFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		cBFrame.setVisible(true);
+		cBFrame.setSize(350, 150);
+	}
+}
